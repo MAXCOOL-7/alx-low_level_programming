08046b5f242a2819ef605bd2e6a5e60d839456eb
MAXCOOL-7 <stevenreigns45@gmail.com>
2022-12-09 19:06:20 -0800

    2.task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -print alpha in lowers case, follwed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char ma;
+
+for (ma = 'a'; ma <= 'z'; ma++)
+{
+putchar(ma);
+}
+putchar("\n");
+return (0);
+}
