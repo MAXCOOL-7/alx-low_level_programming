8cc1f233fc927c2343c3ca935bac38a48255ebe1
MAXCOOL-7 <stevenreigns45@gmail.com>
2022-12-09 22:09:21 -0800

    9.task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int n;
+int n;
 
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-		if (n != 57)
-	{
-		putchar(',');
-		putchar(' ');
-	}
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
 putchar('\n');
 return (0);
 }
