d2b4efdcad5eca972d77c9e0e7241e108ad2ec32
MAXCOOL-7 <stevenreigns45@gmail.com>
2022-12-14 13:49:42 -0800

    8.task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,40 +1,32 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints 24 hours
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: ...
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int a, b, c, d;
 
-	for (i = 0; i < 24; i++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (j = 0; j < 60; j++)
-		{
-			if (i < 10)
-			{
-				_putchar('0');
-				_putchar(i + '0');
-			}
-			else if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			if (j < 10)
-			{
-				_putchar(':');
-				_putchar('0');
-				_putchar(j + '0');
-			}
-			else if (j > 10)
-			{
-				_putchar(':');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
-			_putchar('\n');
-		}
-
+	for (b = 0; b <= 9; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(50);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
 	}
 }
