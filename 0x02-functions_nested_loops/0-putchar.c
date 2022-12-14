8c76800847457d959e56d881976a27934bbf40ed
MAXCOOL-7 <stevenreigns45@gmail.com>
2022-12-13 17:11:26 -0800

    0.task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#inlude "main.h"
 
 /**
- * main -  prints _putchar
+ * main -prints _putchar
  *
  * Return: 0
  */
@@ -10,10 +10,10 @@ int main(void)
 _putchar('-');
 _putchar('p');
 _putchar('u');
+_putchar('t');
 _putchar('c');
 _putchar('h');
 _putchar('a');
-_putchar('r');
 _putchar('r');
 _putchar('\n');
 return (0);
