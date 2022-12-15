4b5c7c0236c89a0af09508ba2b06444b4c94c178
MAXCOOL-7 <stevenreigns45@gmail.com>
2022-12-14 16:42:05 -0800

    10.task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * add -returns the sum of the parameters
+ * add - returns the sum of its  parameters
  * @a: int type number
- * @b: in type number
+ * @b: int type number
+ * Return: 0
  */
-int add(int, int);
+int add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
