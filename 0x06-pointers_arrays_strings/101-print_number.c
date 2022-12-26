8044383d623930e73fa98eaf8eeb0c363e97b48d
Maxcool <97757861+MAXCOOL-7@users.noreply.github.com>
2022-12-26 21:52:42 +0100

    9.task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,26 @@
-include 
+#include "main.h"
+
 /**
  * print_number - print numbers chars
  * @n: integer params
  * Return: 0
  */
-                           void print_number(int n)
-{
-                       unsigned int n1;
-n                1 = n;
-                  if (n < 0)
-{    
-_putchar('-')
-n1 =          -n;                                  
-}
-                if (n1 / 10 != 0)
-{
-        print_                                         number(n1 / 10);
-}
-_ putchar((n1                                             % 10) + '0'       )                 
 
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
