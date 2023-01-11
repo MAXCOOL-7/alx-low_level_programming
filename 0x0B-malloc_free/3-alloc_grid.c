26f32e6ea654b259c390233915dfe8094b0a8480
Maxcool <97757861+MAXCOOL-7@users.noreply.github.com>
2023-01-11 04:25:13 +0100

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,31 @@
  */
 int **alloc_grid(int width, int height)
 {
-  int **array, i, j;
-  int len = width * height;
+int **array, i, j;
+int len = width * height;
 
-  if (len <= 0)
-  return (NULL);
+if (len <= 0)
+return (NULL);
 
-  array = (int **)malloc(sizeof(int *) * height);
-  if (array == NULL)
-  return (NULL);
+array = (int **)malloc(sizeof(int *) * height);
+if (array == NULL)
+return (NULL);
 
-  for (i = 0; i < height; i++)
-  {
-  array[i] = (int *)malloc(sizeof(int) * width);
-  if (array[i] == NULL)
-  {
-  for (i--; i >= 0; i--)
-  free(array[i]);
-  free(array);
-  return (NULL);
-  }
-  }
+for (i = 0; i < height; i++)
+{
+array[i] = (int *)malloc(sizeof(int) * width);
+if (array[i] == NULL)
+{
+for (i--; i >= 0; i--)
+free(array[i]);
+free(array);
+return (NULL);
+}
+}
 
-  for (i = 0; i < height; i++)
-  for (j = 0; j < width; j++)
-  array[i][j] = 0;
+for (i = 0; i < height; i++)
+for (j = 0; j < width; j++)
+array[i][j] = 0;
 
-  return (array);
+return (array);
 }
