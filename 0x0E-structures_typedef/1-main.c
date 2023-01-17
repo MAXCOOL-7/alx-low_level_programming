42699c46be3b847f765567678ca6b95ed6031b1c
Maxcool <97757861+MAXCOOL-7@users.noreply.github.com>
2023-01-17 14:26:45 +0100

    1.task

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* init_dog - initializes a structure of type dog
+* @d: pointer to structure
+* @name: pointer to the name of the dog
+* @age: age of dog
+* @owner: pointer to owner of dog
+*
+* Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
